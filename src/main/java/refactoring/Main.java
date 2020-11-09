e3c5297081b2d898264cdfff35665f0698d1a223
oab <37352140+oab@users.noreply.github.com>
2020-11-09 14:04:43 +0100

    Fixed library import

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -1,7 +1,10 @@
 package refactoring;
 
+import org.abs_models.frontend.parser.*;
+
 public class Main {
-    public static void  main(String args[]) {
-        System.out.println("check");
+    public static void  main(String[] args) {
+
+        System.out.println("Owl placeholder");
     }
 }
