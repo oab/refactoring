37875b873cd26c913e4febd2015c22595dbe2907
oab <37352140+oab@users.noreply.github.com>
2020-11-09 17:11:19 +0100

    update

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -2,9 +2,12 @@ package refactoring;
 
 import org.abs_models.frontend.parser.*;
 
+
 public class Main {
-    public static void  main(String[] args) {
+    public static void  main(String... args) {
 
         System.out.println("Owl placeholder");
+        org.abs_models.frontend.parser.Main.main(args);
+
     }
 }
