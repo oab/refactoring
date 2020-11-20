a9e5bb6e7e2a985691b4988989565e8e3ee6778f
oab <37352140+oab@users.noreply.github.com>
2020-11-20 14:20:26 +0100

    Slight changes, tree changes coming

--- src/main/java/refactoring/Refactor.java
+++ src/main/java/refactoring/Refactor.java
@@ -1,14 +1,18 @@
 package refactoring;
 
 import org.abs_models.backend.common.InternalBackendException;
+import org.abs_models.backend.prettyprint.ABSFormatter;
+import org.abs_models.backend.prettyprint.DefaultABSFormatter;
 import org.abs_models.common.WrongProgramArgumentException;
 import org.abs_models.frontend.ast.Model;
 import org.abs_models.frontend.parser.Main;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.io.File;
 import java.util.ArrayList;
+import java.io.PrintWriter;
 
 public class Refactor {
 
@@ -21,14 +25,33 @@ public class Refactor {
     // args[2] call at line (e.g. 3)
     // args[3]... file(s)
     public static void main(String args[]) {
-        File in = new File(args[0]);
+
+        // TODO: Proper argument handling?
+        if(args.length != 3) {
+            System.out.println("Expected: Class Method File");
+            System.exit(1);
+        }
+
+
+
         ArrayList<File> files  = new ArrayList<>();
+        File in = new File(args[2]);
+
+        // TODO: regexp match and rewrite *.abs -> *.after.abs
+        File out = new File(args[2]+".after");
         files.add(in);
 
         Main entry = new Main();
+
+        //TODO: Is the exception handling really needed?
         try {
+
             Model m = entry.parse(files);
-            m.lookupModule("HideDelegate").getDecl(3).dump(System.out);
+            PrintWriter writer = new PrintWriter(out);
+            ABSFormatter formatter = new DefaultABSFormatter(writer);
+
+            hideDelegate(m,args[0],args[1]).doPrettyPrint(writer,formatter);
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (WrongProgramArgumentException e) {
@@ -40,8 +63,11 @@ public class Refactor {
 
     }
 
-    // implement refactoring i.e. Model -> transformed Model -> prettyprint & emit as file
-    public static Model hideDelegate(Model m) {
+    // TODO: implement refactoring i.e. Model -> transformed Model
+    // 1. Traverse to class
+    // 2. Traverse to method in class
+    public static Model hideDelegate(Model m, String classname, String methodname) {
+
 
         return m;
     }
