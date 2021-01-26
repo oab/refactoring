f1fa82f595bcb18347c967fad142d6c712b6db22
oab <37352140+oab@users.noreply.github.com>
2021-01-26 15:59:04 +0100

    Simple replacement operation implemented

--- src/main/java/refactoring/Refactor.java
+++ src/main/java/refactoring/Refactor.java
@@ -1,68 +1,22 @@
 package refactoring;
 
-import org.abs_models.backend.common.InternalBackendException;
 import org.abs_models.backend.prettyprint.ABSFormatter;
 import org.abs_models.backend.prettyprint.DefaultABSFormatter;
-import org.abs_models.common.WrongProgramArgumentException;
 import org.abs_models.frontend.ast.*;
-import org.abs_models.frontend.parser.Main;
 import org.abs_models.frontend.typechecker.KindedName;
-
-import java.io.IOException;
-import java.util.Collections;
 import java.io.PrintWriter;
-import java.io.File;
 import java.util.Iterator;
 
 public class Refactor {
 
-    // For now let's just hardcode some stuff to get going,
-    // but probably should use the cmdline parsing library
-    // already in use if this progresses beyond a one-off thing
-
-    // args[0] Module
-    // args[1] Class
-    // args[2] Method
-    // args[3] varname1
-    // args[4] varname2
-    // args[5]... file(s)
-    public static void main(String args[]) {
-
-        // TODO: Proper argument handling?
-        if(args.length != 7) {
-            System.out.println("Expected: Module Class Method AssignVar TargetVar MethodCall File");
-            System.exit(1);
-        }
-
-        Main entry = new Main();
-
-        //TODO: Is the exception handling really needed?
-        try {
-
-            Model m = entry.parse(Collections.singletonList(new File(args[6])));
-            PrintWriter writer = new PrintWriter(new File(args[6]+".after"));
-            ABSFormatter formatter = new DefaultABSFormatter(writer);
-
-            hideDelegate(m,args[0],args[1],args[2],args[3],args[4],args[5])
-                    .doPrettyPrint(writer,formatter);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (WrongProgramArgumentException e) {
-            e.printStackTrace();
-        } catch (InternalBackendException e) {
-            e.printStackTrace();
-        } catch (RefactoringException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
     // TODO: implement refactoring i.e. Model -> transformed Model
-    public static Model hideDelegate(Model m, String moduleName, String className, String methodName,
-                                     String assignVar, String targetVar, String methodCall)
-            throws RefactoringException {
+    public static Model hideDelegate(Model m,
+                                     String moduleName,
+                                     String className,
+                                     String methodName,
+                                     String assignVar,
+                                     String targetVar,
+                                     String methodCall ) throws RefactoringException {
 
         ModuleDecl mDecl = m.lookupModule(moduleName);
         if (mDecl == null) {
@@ -82,33 +36,21 @@ public class Refactor {
         // Matching against e.g
         // 1. d = p.getDept();
         // 2. m = d.getManager();
+        // Assume p also implements getManager (TODO assume -> ensure)
+        // remove 1. and 2. and replace with 3.
+        // 3. m = p.getManager();
 
-        Iterator<Stmt> stmts = mImpl.getBlock().getStmts().iterator();
+        //Iterator<Stmt> stmts = mImpl.getBlock().getStmts().iterator();
+        List<Stmt> stmtlist = mImpl.getBlock().getStmtList();
 
-        // Match 1.
-        /* Java 14
-        while (stmts.hasNext()) {
-            if(stmts.next() instanceof AssignStmt astmt1 &&
-               astmt1.getVar().getName().equals(assignVar) &&
-               astmt1.getValue() instanceof SyncCall syncallstmt1 &&
-               syncallstmt1.getMethod().equals(methodCall) &&
-               syncallstmt1.getCalleeNoTransform() instanceof VarUse v1 &&
-               v1.getName().equals(targetVar) &&
-               stmts.next() instanceof AssignStmt astmt2 &&
-               astmt2.getValue() instanceof SyncCall syncallstmt2 &&
-               syncallstmt2.getCalleeNoTransform() instanceof VarUse v2 &&
-               v2.getName().equals(assignVar)) {
-                return m;
-            }
-        }
-        */
+        MatchAndReplace: {
+            Iterator<Stmt> stmts = stmtlist.iterator();
+            Stmt match1;
+            Stmt match2;
 
-        Stmt match1;
-        Stmt match2;
-        Match: {
+
             while (stmts.hasNext()) {
                 match1 = stmts.next();
-
                 if (!(match1 instanceof AssignStmt)) continue;
                 AssignStmt astmt1 = (AssignStmt) match1;
 
@@ -136,18 +78,28 @@ public class Refactor {
 
                 if(!v2.getName().equals(assignVar)) continue;
 
-                break Match;
+                // Construct the replacement node
+                SyncCall call = new SyncCall()
+                        .setMethod(syncallstmt2.getMethod())
+                        .setCallee(new VarUse().setName(targetVar));
+
+                AssignStmt replacement = new AssignStmt()
+                        .setVar(new VarUse().setName(astmt2.getVar().getName()))
+                        .setValue(call);
+
+                match1.replaceWith(replacement);
+
+                // Remove the line after the replacement
+                stmtlist.removeChild(stmtlist.getIndexOfChild(match2));
+
+
+                break MatchAndReplace;
 
             }
 
             // Not found
             throw new RefactoringException(String.format("No match found"));
         }
-
-        PrintWriter writer = new PrintWriter(System.out);
-        ABSFormatter formatter = new DefaultABSFormatter(writer);
-        match1.doPrettyPrint(writer,formatter);
-        match2.doPrettyPrint(writer,formatter);
 
         return m;
 
